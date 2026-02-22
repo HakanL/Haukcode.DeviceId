8e8c8537211055499f45c916fb7262a7abde59ac
Matthew King <mking@mking.net>
2026-02-23 07:10:56 +0800

    Update namespace imports in merged code.

--- src/DeviceId.Linux/Components/CpuInfoIdComponent.cs
+++ src/DeviceId.Linux/Components/CpuInfoIdComponent.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
-using DeviceId.Internal.CommandExecutors;
+using DeviceId.CommandExecutors;
 
 namespace DeviceId.Linux.Components;
 
