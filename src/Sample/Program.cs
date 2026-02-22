1aa3b2145277a7c042ffdc0558d3dcdd79cd9d55
Hakan Lindestaf <hakan@lindestaf.com>
2026-02-22 14:47:13 -0600

    Trim and added sample

---
+++ src/Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using DeviceId;
+using DeviceId.Encoders;
+using DeviceId.Formatters;
+
+Console.WriteLine("Getting Device Id");
+var deviceIdManager = new DeviceIdManager()
+    .AddBuilder(1, builder => builder
+        .OnWindows(windows => windows
+            .AddMachineGuid()
+            .AddProcessorId()
+            .AddMotherboardSerialNumber()
+            .AddSystemDriveSerialNumber())
+        .OnLinux(linux => linux
+            .AddMachineId()
+            .AddCpuInfo()
+            .AddMotherboardSerialNumber()
+            .AddSystemDriveSerialNumber())
+        //.OnMac(mac => mac
+        //    .AddSystemDriveSerialNumber()
+        //    .AddPlatformSerialNumber())
+        .UseFormatter(new XmlDeviceIdFormatter(new PlainTextDeviceIdComponentEncoder())));
+
+
+Console.WriteLine($"Device Id = {deviceIdManager.GetDeviceId()}");
